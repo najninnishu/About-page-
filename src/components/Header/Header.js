60c646a6d2c316108232767a56b1f8d7c8345fc6
Najnin Nishu <najninnishu2020@gmail.com>
2023-03-31 00:22:52 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+
+
+
+            <header class="header">
+
+                <img src="" alt="" srcset="" />
+
+                <nav class="navbar">
+                    <a href="#Work">Work</a>
+                    <a href="#product">Product</a>
+                    <a href="#Academy">Academy</a>
+                    <a href="#Survey">Survey</a>
+                    <a href="#Blogs">Blogs</a>
+                    <a href="#Contact">Contact</a>
+
+                </nav>
+                {/* <button class="c"> contact</button> */}
+            </header>
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/body/Body.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import './Body.css';
+const Body = () => {
+    return (
+        <div>
+
+            <div class="part1">
+
+                <h4>Who we are?</h4>
+
+                <p>Carriastic is trying to solve the Unemployment problem initially in
+                    Bangladesh and later on globally. We are basically a job placement
+                    platform for graduates and understudies, especially, focusing on
+                    their careers for ensuring job placement in the competitive job
+                    market.</p>
+                <button class="c">Learn More</button>
+
+
+
+            </div>
+
+            <div class="part2">
+
+                <h4>Our Mission</h4>
+
+                <p>Our mission is to ensure qualified candidates for our stakeholders
+                    and make the candidates eligible to start their very first career
+                    through various skill development programs. Our main focus is to
+                    create a platform that can create a bridge among the concerns and
+                    applicants.</p>
+
+            </div>
+
+            <div class="part3">
+
+                <h4>Our Vision</h4>
+
+                <p>Our vision is to create a sustainable qualified human resource to
+                    reduce the unemployment problem nationally and globally. Though
+                    initially we are trying to reduce the unemployment problem in
+                    Bangladesh, we will expand our network to an international level.
+                    We will ensure local employment, global employment, and remote
+                    jobs as well. Moreover, our organization will create a positive effect
+                    on the global or local environment, community, society, or
+                    economy.</p>
+
+            </div>
+
+
+            <div class="part4">
+
+                <h4>Why Us?</h4>
+
+                <p>.Carriastic is trying to solve the Unemployment problem initial in Bangladesh and later on globally.
+                </p>
+                <p>.Carriastic will become the bridge between job seekers and employee seekers(vendors). Job seekers will get their desired job and vendors will be ensured qualified employees without hassle.
+
+                </p>
+                <p>.As a result, productivity of vendors will be increased and unemployment problem will be slightly reduced.
+                </p>
+                <p>.Initially we will collect jobs seekers by CV collection. We are collecting CV/resumes by various way like collecting from our own CV
+                    dropping platform, social media ads, in person communication etc.</p>
+                <p>.Later on, we will conduct career development program in universities and collect proper candidates by running a 4-month training program.
+                </p>
+                <p>.These trained qualified candidates will be referred to the vendors.
+                </p>
+            </div>
+
+
+            <div class="part5">
+
+                <h1>Interested On Us?</h1>
+
+                <button class="c">Contact Now</button>
+
+
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Body;
